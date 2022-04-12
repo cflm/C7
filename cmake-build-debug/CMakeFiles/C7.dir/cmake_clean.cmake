d2cb3f8849fc40ec71e5e613c6cb0fce47c86ac3
cflm <208625250@qq.com>
2022-04-13 01:45:42 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/C7.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "C7"
+  "C7.pdb"
+  "CMakeFiles/C7.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
